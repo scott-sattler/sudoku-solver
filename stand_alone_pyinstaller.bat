57a2009f2c633a7cc3bfd75972bc1345cad87de0
Scott Sattler <scott.sattler.dev@gmail.com>
2024-04-08 21:12:39 -0500

    reorg

--- stand_alone_pyinstaller.bat
+++
@@ -1,8 +0,0 @@
-c:\
-cd c:\python27\scripts
-start pyinstaller C:/Users/Admin/Dropbox/Sudoku/gui_32.py^
- --hidden-import=Tkinter^
- --onefile --noconsole --clean^
- --icon="C:/Users/Admin/Dropbox/Sudoku/sudoku.ico"^
- --distpath="C:/Users/Admin/Dropbox/Sudoku/dist"^
- --name=Sudoku --specpath="C:/Users/Admin/Dropbox/Sudoku/"
