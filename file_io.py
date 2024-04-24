69134f95f7c616631c7394965657ddf0e220615e
Scott Sattler <scott.sattler.dev@gmail.com>
2024-04-24 05:40:26 -0500

    changed file format; added save/load functionality; fixed index error

--- file_io.py
+++ file_io.py
@@ -6,8 +6,11 @@ import random as r
 class FileIO:
     """ non-data line prefix '_' """
     DATA_FILE_NAME = '17puz49158.txt'
+    SAVE_FILE_NAME = 'sudoku_save'
+    BOARD_DATA_SENTINEL_VALUE = 'board_data:'
+    CLUE_17_SENTINEL_VALUE = '_17_hint_set: '
 
-    def __init__(self, local_save_path='./', save_file_name='sudoku_save'):
+    def __init__(self, local_save_path='./', save_file_name=SAVE_FILE_NAME):
         self.save_path = local_save_path
         self.save_file_name = save_file_name + '.txt'
         if local_save_path == './':
@@ -27,47 +30,16 @@ class FileIO:
     def create_file(self):
         try:
             f = open(self.save_path, 'x')
-            # f.write('_17_hint_set: \n')  # todo: fully implement
+            f.write(self.CLUE_17_SENTINEL_VALUE + '\n\n')
             f.close()
             logging.info(f'file creation successful:\n\t{self.save_path}')
-        except (Exception,):
+        except (IOError,):
             logging.warning(f'file creation failure:\n\t{self.save_path}')
-
-    def write_board_to_save_file(self, board_data):
-        try:
-            with open(self.save_path, 'a') as f:
-                encoded = self.convert_to_formatted_str(board_data)
-                line = encoded + '\n'
-                f.write(line)
-                f.close()
-                logging.info(f'write successful:\n\t{self.save_path}\n\t{encoded}')
-        except (Exception,) as e:
-            logging.exception('write failure', stack_info=True)
-            return False
-        return True
-
-    def read_and_load_all_boards_from_save_file(self):
-        """ returns a list of boards """
-        all_data = list()
-        try:
-            with open(self.save_path, 'r') as f:
-                lines = f.readlines()
-                for line in lines:
-                    if line and line[0] == '_':
-                        continue
-                    stripped_suffix = line.rstrip("\n\r")
-                    decoded = self.convert_from_formatted_str(stripped_suffix)
-                    all_data.append(decoded)
-                logging.info('save read successful')
-        except (Exception,) as e:
-            logging.exception('save read failure', stack_info=True)
-            return False
-        return all_data
 
     def read_and_load_board_from_17_hints_data_file(self):
         """ returns a list of boards """
         board_save_data = str()
-        random_int = r.randint(1, 49158 + 1)
+        random_int = r.randint(0, 49158 - 1)
         try:
             with open(self.data_path_17_hints, 'r') as f:
                 for i, line in enumerate(f):
@@ -75,33 +47,13 @@ class FileIO:
                         board_save_data = line.rstrip("\n\r")
                         break
             logging.info(f'17 clue data read successful:\n\t{board_save_data}')
-        except (Exception,) as e:
+        except (IOError,) as e:
             logging.exception('save read failure', stack_info=True)
             return False
-        return self.convert_from_formatted_str(board_save_data)
-
-    def convert_to_formatted_str(self, board_to_convert):
-        return self.board_to_str(board_to_convert)
-
-    def convert_from_formatted_str(self, save_data):
-        return self.str_to_board(save_data)
+        return self.str_to_board_without_notes(board_save_data)
 
     @staticmethod
-    def board_to_str(board):
-        """ converts from array format to string format """
-        n, m = len(board), len(board[0])
-        to_str = list()
-        for i in range(n):
-            for j in range(m):
-                char = str(board[i][j])
-                if char == '0':
-                    char = '.'
-                to_str.append(char)
-        to_str = ''.join(to_str)
-        return to_str
-
-    @staticmethod
-    def str_to_board(str_data) -> list[list[int]]:
+    def str_to_board_without_notes(str_data: str) -> list[list[int]]:
         """ converts from string format to array format """
         n = m = 9  # assumes 9 by 9
 
@@ -119,7 +71,190 @@ class FileIO:
                 row = list()
                 j = 0
 
+        print(reconstructed)
         return reconstructed
+
+    ###########################################################################
+    # save/load fns
+
+    def write_board_to_save_file(self, board_data):
+        try:
+            with open(self.save_path, 'a') as f:
+                encoded = self.BOARD_DATA_SENTINEL_VALUE + '\n'
+                encoded = encoded + self.board_with_notes_to_str(board_data)
+                for line in encoded.split('\n'):
+                    line = line + '\n'
+                    f.write(line)
+                f.write('\n')
+                f.close()
+            logging.info(f'write successful:\n\t{self.save_path}\n\t{encoded}')
+        except (IOError,) as e:
+            logging.exception('write failure', stack_info=True)
+            return False
+        return True
+
+    def read_saved_board_from_save_file(self):
+        """ reads str format and returns converted 3d note format """
+        b_d_s_v = self.BOARD_DATA_SENTINEL_VALUE
+        board_lines_val_notes_state: list[str] = list()
+        try:
+            with open(self.save_path, 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    if line and line[0] == '_':
+                        continue
+                    stripped_suffix = line.rstrip("\n\r")
+                    if stripped_suffix == '':
+                        continue
+                    board_lines_val_notes_state.append(stripped_suffix)
+
+            for i, data_line in enumerate(board_lines_val_notes_state):
+                if b_d_s_v in data_line:
+                    break
+            board_lines_val_notes_state = board_lines_val_notes_state[i + 1:]
+            logging.info('save read successful')
+        except (IOError,) as e:
+            logging.exception('save read failure', stack_info=True)
+            return False
+        return self.str_lines_to_board_with_notes(board_lines_val_notes_state)
+
+    @staticmethod
+    def board_with_notes_to_str(board: list[list[list[int]]]) -> str:
+        """
+            converts from 3d CellData matrix format to string format
+            board_str
+            note_str_1
+            note_str_2
+            ...
+            note_str_9
+            locked_state
+        """
+        n, m = len(board), len(board[0])
+
+        locked_state = list()
+        for i in range(n):
+            for j in range(m):
+                char = str(board[i][j][-1])
+                locked_state.append(char)
+        locked_state = ''.join(locked_state)
+
+        board_str = list()
+        for i in range(n):
+            for j in range(m):
+                char = str(board[i][j][0])
+                if char == '0':
+                    char = '.'
+                board_str.append(char)
+        board_str = ''.join(board_str)
+
+        # zero-indexed
+        notes_strs = [list() for _ in range(1, 10)]
+        for i in range(n):
+            for j in range(m):
+                notes = board[i][j][:]  # todo
+                for k in range(1, 10):
+                    note = str(notes[k])
+                    if note == '0':
+                        note = '.'
+                    notes_strs[k - 1].append(note)
+
+        notes_strs = '\n'.join([''.join(note_str) for note_str in notes_strs])
+
+        return board_str + '\n' + notes_strs + '\n' + locked_state
+
+    @staticmethod
+    def str_lines_to_board_with_notes(str_line_data: list[str]) \
+            -> tuple[list[list[list[int]]], list[list[int]]]:
+        """ returns tuple of 3d board with notes and 2d lock state board """
+        board_val_data = str_line_data[0]
+        note_val_data = str_line_data[1:-1]
+        locked_state_data = str_line_data[-1]
+        reconstructed_board_with_notes = list()
+        locked_state_board = list()
+
+        # board value reconstruction
+        i = j = 0
+        row = list()
+        for char in board_val_data:
+            if char == '.':
+                char = 0
+            row.append([int(char)])
+            j += 1
+            if j > 8:
+                j = 0
+                i += 1
+                reconstructed_board_with_notes.append(row)
+                row = list()
+
+        # note reconstruction
+        i = j = 0
+        for x in range(len(note_val_data[0])):
+            cell_notes = list()
+            for k in range(9):
+                char = note_val_data[k][x]
+                if char == '.':
+                    char = 0
+                char = int(char)
+                cell_notes.append(char)
+            val = reconstructed_board_with_notes[i][j]
+            reconstructed_board_with_notes[i][j] = val + cell_notes
+            j += 1
+            if j > 8:
+                j = 0
+                i += 1
+
+        # locked state reconstruction
+        i = j = 0
+        row = list()
+        for char in locked_state_data:
+            row.append(int(char))
+            j += 1
+            if j > 8:
+                j = 0
+                i += 1
+                locked_state_board.append(row)
+                row = list()
+
+        return reconstructed_board_with_notes, locked_state_board
+
+    ###########################################################################
+
+    @staticmethod
+    # used for logging generated boards
+    def board_without_notes_to_str(board: list[list[int]]) -> str:
+        """ converts from array format to string format """
+        n, m = len(board), len(board[0])
+        to_str = list()
+        for i in range(n):
+            for j in range(m):
+                char = str(board[i][j])
+                if char == '0':
+                    char = '.'
+                to_str.append(char)
+        to_str = ''.join(to_str)
+        return to_str
+
+    ###########################################################################
+    # todo: unused below
+
+    # todo: review/remove; unused
+    def read_and_load_all_boards_from_save_file(self):
+        """ returns a list of boards """
+        all_data = list()
+        try:
+            with open(self.save_path, 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    if line and line[0] == '_':
+                        continue
+                    stripped_suffix = line.rstrip("\n\r")
+                    decoded = self.str_to_board_without_notes(stripped_suffix)
+                    all_data.append(decoded)
+            logging.info('save read successful')
+        except (IOError,) as e:
+            logging.exception('save read failure', stack_info=True)
+            return False
+        return all_data
 
 
 if __name__ == '__main__':
@@ -143,4 +278,3 @@ if __name__ == '__main__':
     test_convert = io.read_and_load_all_boards_from_save_file()
     # print(io._board_to_str(test_convert))
     # print(u.strip_for_print(test_convert))
-
